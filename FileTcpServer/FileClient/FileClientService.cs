ce1c2ac4f3f28e736ce024d53d5c353a8bc165a9
jacob9 <jakub.krzysztof.czapski@gmail.com>
2016-03-02 12:34:31 +0100

    project is kinda working

---
+++ FileTcpServer/FileClient/FileClientService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using PasswordCrackerCentralized;
+
+namespace FileClient
+{
+    internal class FileClientService
+    {
+        private int SERVER_PORT;
+
+        public FileClientService(int SERVER_PORT)
+        {
+            this.SERVER_PORT = SERVER_PORT;
+        }
+
+        public void Start()
+        {
+            Trace.TraceInformation("Client started");
+
+            Console.Write("Type filename: ");
+            String fileToReceive = Console.ReadLine();
+            Console.WriteLine();
+
+            using (TcpClient client = new TcpClient("localhost", SERVER_PORT))
+            {
+                using (Stream stream = client.GetStream())
+                {
+                    StreamWriter writer = new StreamWriter(stream);
+                    writer.WriteLine(fileToReceive);
+                    writer.Flush();
+                    
+                    using (FileStream fileStream = new FileStream(@"C:\Files\FileClient.txt", FileMode.Create))
+                    {
+                        stream.CopyTo(fileStream);
+                    }
+                  
+                }
+            }
+
+        }
+    }
+}
--- PasswordcrackerWebService/CrackingWebService/obj/Release/Package/PackageTmp/CrackingWS.asmx
+++
@@ -1,1 +0,0 @@
-﻿<%@ WebService Language="C#" CodeBehind="CrackingWS.asmx.cs" Class="WebApplication1.CrackingWS" %>
